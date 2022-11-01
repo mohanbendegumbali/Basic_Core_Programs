aa7e29f1ec9e1c2888b2b9fa8e7f1a8ecc4b4d57
mohanbendegumbali <mohan0311@hotmail.com>
2022-11-01 18:40:37 +0530

    [Mohan] Add: Folder Structure Created

---
+++ BasicCorePrograms/EvenOdd.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class EvenOdd
+    {
+        public void calculate()
+        {
+            Console.WriteLine(" Enter the number ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+                Console.WriteLine(" The given number is even ");
+            else
+                Console.WriteLine(" The given number is odd ");
+        }
+    }
+}
