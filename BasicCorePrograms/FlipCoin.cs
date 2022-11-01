5ff3b1104083d066070ab82bf0d446aca8e4974d
mohanbendegumbali <mohan0311@hotmail.com>
2022-11-01 17:20:53 +0530

    [Mohan] Add: Folder Structure Created

---
+++ BasicCorePrograms/FlipCoin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class FlipCoin
+    {
+        Random random = new Random();
+        public void Coin()
+        {
+            int head = 0, tail = 0;
+            Console.WriteLine(" Enter the number of times coin to be flipped ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int actualflip = num;
+            while (num > 0)
+            {
+                if (random.NextDouble() > 0.5)
+                    head++;
+                else
+                    tail++;
+                num--;
+            }
+            double headpercent = (head / actualflip) * 100;
+            double tailpercent = (tail / actualflip) * 100;
+            Console.WriteLine(" HeadPercentage : " + headpercent);
+            Console.WriteLine(" TailPercentage : " + tailpercent);
+        }
+    }
+}
---
+++ BasicCorePrograms/Program.cs
@@ -0,0 +1,26 @@
+﻿using BasicCorePrograms;
+using System; 
+
+namespace Demo 
+{
+    class Program 
+    {
+        public static void Main(string[] args) 
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.Flip Coin" + "\n" + "2.Leap Year" + "\n" + "3.Swapping" + "\n" + "4.Largest" + "\n" + "5.Vowels" + "\n" + "6.Power" + "\n" + "7.Even or Odd ");
+                Console.WriteLine(" Enter the option ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin flip = new FlipCoin();
+                        flip.Coin();
+                        break;
+                }
+            }
+        }
+    }
+}
